78d2296bb4440a5650b6ee68d1c221fbf36128fc
Harsh Manekar <harshansh222@gmail.com>
2025-12-25 11:10:48 +0530

    Distribute Candies

---
+++ src/Problems/DistributeCandies.java
@@ -0,0 +1,97 @@
+package Problems;
+
+
+/*
+        Problem Description
+
+
+
+        N children are standing in a line. Each child is assigned a rating value.
+        You are giving candies to these children subjected to the following requirements:
+        Each child must have at least one candy.
+        Children with a higher rating get more candies than their neighbors.
+        What is the minimum number of candies you must give?
+
+
+
+        Problem Constraints
+        1 <= N <= 105
+        -109 <= A[i] <= 109
+
+
+
+
+        Input Format
+        The first and only argument is an integer array A representing the rating of children.
+
+        Output Format
+        Return an integer representing the minimum candies to be given.
+
+        Example
+
+        Input 1: A = [1, 2]
+        Input 2: A = [1, 5, 2, 1]
+
+        Output 1: 3
+        Output 2: 7
+
+
+        Example
+        Explanation 1:
+
+         The candidate with 1 rating gets 1 candy and candidate with rating 2 cannot get 1 candy as 1 is its neighbor.
+         So rating 2 candidate gets 2 candies. In total, 2 + 1 = 3 candies need to be given out.
+*/
+
+
+
+
+
+
+import java.util.*;
+
+public class DistributeCandies {
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> alist = new ArrayList<>();
+        alist.add(1);
+        alist.add(2);
+        alist.add(3);
+        alist.add(4);
+
+        System.out.println(Logic(alist));
+    }
+
+    public static int Logic(ArrayList<Integer> A)
+    {
+
+        int n = A.size();
+
+        int[] arr = new int[n];
+
+        for(int i = 1; i<n ; i++)
+        {
+            if(A.get(i) > A.get(i-1))
+            {
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+
+        for(int i = n -2; i>=0; i--)
+        {
+            if(A.get(i) > A.get(i+1))
+            {
+                arr[i] = Math.max(arr[i], arr[i + 1] + 1);
+            }
+        }
+
+        int sum = n;
+        for(int i : arr)
+        {
+            sum = sum + i;
+        }
+
+        return sum;
+    }
+}
--- src/Problems/GasStation.java
+++ src/Problems/GasStation.java
@@ -4,13 +4,13 @@ Problem Description
 
 
 
-Given two integer arrays A and B of size N. There are N gas stations along a circular route, where the amount of gas at station i is A[i].
-
-You have a car with an unlimited gas tank and it costs B[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
-
+Given two integer arrays A and B of size N. There are N gas stations along a circular route,
+where the amount of gas at station i is A[i].
+You have a car with an unlimited gas tank and it costs B[i] of gas to travel from station i to its next station (i+1).
+You begin the journey with an empty tank at one of the gas stations.
 Return the minimum starting gas station's index if you can travel around the circuit once, otherwise return -1.
-
-You can only travel in one direction. i to i+1, i+2, ... n-1, 0, 1, 2.. Completing the circuit means starting at i and ending up at i again.
+You can only travel in one direction. i to i+1, i+2, ... n-1, 0, 1, 2..
+Completing the circuit means starting at i and ending up at i again.
 
 
 Problem Constraints
@@ -44,7 +44,8 @@ Now your tank has 1 unit of gas. But you need B[0] = 2 gas to travel to station 
 If you start from index 1, you can fill in A[1] = 2 amount of gas.
 Now your tank has 2 units of gas. You need B[1] = 1 gas to get to station 0.
 So, you travel to station 0 and still have 1 unit of gas left over.
-You fill in A[0] = 1 unit of additional gas, making your current gas = 2. It costs you B[0] = 2 to get to station 1, which you do and complete the circuit.
+You fill in A[0] = 1 unit of additional gas, making your current gas = 2.
+It costs you B[0] = 2 to get to station 1, which you do and complete the circuit.
  */
 
 
@@ -78,10 +79,6 @@ public class GasStation {
         {
             blist.add(sc1.nextInt());
         }
-
-
-
-
 
         System.out.println(CanCompleteCircuit(alist, blist));
     }
